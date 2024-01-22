c3d8c82760ce216db514f560c5efd934eccc4bfb
SINH ROHITSINH DINESHSINH <138886640+Srd8887@users.noreply.github.com>
2024-01-22 19:42:55 +0530

    SIMPLE CHATBOT

---
+++ SIMPLE CHATBOT.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import time
+now = time.ctime()
+
+Qna ={
+    "Hi":"Hey" ,
+    "How are you":"I am fine",
+    "what is your name":"My name is Abd",
+    "How old are you":"I am 32 year old",
+    "What is Time now": now ,
+}
+
+while True:
+    qt = input()
+    
+    if(qt == "Quit my dear"):
+        break
+        
+    else:
+        print(Qna[qt])
+        
+
